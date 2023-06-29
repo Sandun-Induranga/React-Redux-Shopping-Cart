4196a703dac517031f1c59246eab8107684c9255
Sandun Induranga <sandutharu40@gmail.com>
2023-06-29 12:51:11 +0530

    Item Details Pass from Props

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,10 +1,10 @@
 import React from "react";
 
-const Item = () => {
+const Item = (props) => {
   return (
     <div className="border shadow h-[300px] rounded flex flex-col items-center p-4 gap-2">
-      <h1 className="text-2xl text-slate-700">ITM-001</h1>
-      <h1 className="text-xl text-slate-700">Biscuit</h1>
+      <h1 className="text-2xl text-slate-700">{props.id}</h1>
+      <h1 className="text-xl text-slate-700">{props.name}</h1>
       <button className="bg-slate-700 text-white px-4 py-2 rounded">
         Add to Cart
       </button>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,11 +3,11 @@ import Item from "../components/Item";
 
 const Home = () => {
   return (
-    <div className="grid grid-cols-4 gap-10 p-10">
-      <Item />
-      <Item />
-      <Item />
-      <Item />
+    <div className="grid grid-cols-4 gap-4 p-10">
+      <Item id="ITM-001" name="Biscuit" />
+      <Item id="ITM-002" name="Biscuit" />
+      <Item id="ITM-003" name="Biscuit" />
+      <Item id="ITM-004" name="Biscuit" />
     </div>
   );
 };
