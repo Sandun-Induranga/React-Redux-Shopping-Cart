09eaee7cff2ad1bcb87655ba9422797f11976961
Sandun Induranga <sandutharu40@gmail.com>
2023-06-29 12:27:04 +0530

    Added Tailwind Css to Project

--- src/components/Content.js
+++ src/components/Content.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Home from "../pages/Home";
+import Header from "./Header";
 
 const Content = () => {
   return (
-    <div>
+    <>
+      <Header />
       <Home />
-    </div>
+    </>
   );
 };
 
