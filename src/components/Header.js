818b2ac8fa8d1beea7da9de41bd2fb5b71a97c47
Sandun Induranga <sandutharu40@gmail.com>
2023-06-29 12:39:39 +0530

    Updated Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,10 +3,18 @@ import { NavLink } from "react-router-dom";
 
 const Header = () => {
   return (
-    <div className="w-full h-20 bg-pink-700">
-      <NavLink to="/" className="text-white text-2xl font-bold">
-        Home
-      </NavLink>
+    <div className="w-full h-20 bg-pink-700 flex justify-between items-center px-10">
+      <section>
+        <h1 className="text-white text-xl">Shopping Cart</h1>
+      </section>
+      <section className="flex gap-4">
+        <NavLink to="/" className="text-white">
+          Items
+        </NavLink>
+        <NavLink to="/" className="text-white">
+          Cart
+        </NavLink>
+      </section>
     </div>
   );
 };
