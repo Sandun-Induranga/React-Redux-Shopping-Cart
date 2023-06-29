48b5cdfd05da25b36a54d05fc861aa917fba19fb
Sandun Induranga <sandutharu40@gmail.com>
2023-06-29 12:19:33 +0530

    Header Page Created

--- src/App.js
+++ src/App.js
@@ -2,11 +2,12 @@ import React from "react";
 import { Provider } from "react-redux";
 import store from "./store";
 import ShoppingCart from "./components/ShoppingCart";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <Provider store={store}>
-      <ShoppingCart />
+      <Home />
     </Provider>
   );
 }
