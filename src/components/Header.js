8853a3f07e637bbbd853a6bd10bbc2c4f69c85aa
Sandun Induranga <sandutharu40@gmail.com>
2023-06-29 12:35:42 +0530

    Header Component Updated

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,14 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div className="w-full h-20 bg-pink-700">
+      <NavLink to="/" className="text-white text-2xl font-bold">
+        Home
+      </NavLink>
+    </div>
+  );
 };
 
 export default Header;
