22bac7015def5ef9672934afc60996af3f0933a2
Sandun Induranga <sandutharu40@gmail.com>
2023-06-29 12:48:13 +0530

    Added Content in Items

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,7 +1,15 @@
 import React from "react";
 
 const Item = () => {
-  return <div className="w-[300px] h-[300px] bg-slate-400">Item</div>;
+  return (
+    <div className="border shadow h-[300px] rounded flex flex-col items-center p-4 gap-2">
+      <h1 className="text-2xl text-slate-700">ITM-001</h1>
+      <h1 className="text-xl text-slate-700">Biscuit</h1>
+      <button className="bg-slate-700 text-white px-4 py-2 rounded">
+        Add to Cart
+      </button>
+    </div>
+  );
 };
 
 export default Item;
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,7 +3,7 @@ import Item from "../components/Item";
 
 const Home = () => {
   return (
-    <div>
+    <div className="grid grid-cols-4 gap-10 p-10">
       <Item />
       <Item />
       <Item />
