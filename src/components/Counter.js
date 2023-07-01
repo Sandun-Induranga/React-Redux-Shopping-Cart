1e7d128e19a3679df14dfa31c0ad72ca6fad23f5
Sandun Induranga <sandutharu40@gmail.com>
2023-07-01 23:03:12 +0530

    Updated Component Counter

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,7 +1,15 @@
 import React from "react";
 
 const Counter = () => {
-  return <div>Counter</div>;
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <button onClick={() => setCount(count - 1)}>-</button>
+      <span>{count}</span>
+      <button onClick={() => setCount(count + 1)}>+</button>
+    </div>
+  );
 };
 
 export default Counter;
